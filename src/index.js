1d43b29a05cdc372e430951606da36fc0b819f1f
PassionPI <472121553@qq.com>
2020-01-12 21:01:42 +0800

    npm

--- src/index.js
+++ src/index.js
@@ -4,24 +4,17 @@ const { existsSync, readFileSync } = require('fs')
 
 
 const static = (public = '') => async (ctx, next) => {
-  const name = join(public, ctx.path)
   
-  let body
-  if (existsSync(name)) {
-    try {
-      body = readFileSync(name)
-      ctx.set('Content-Type', mimes[extname(name)] || 'text/plain')
-    } catch {
-      body = '[404] Not Found.'
-      ctx.status = 404
-    }
-  } else {
-    body = '[404] Not Found.'
-    ctx.status = 404
-  }
+  const name = join(public, ctx.path)
 
-  ctx.body = body
-  await next()
+  if (!existsSync(name)) return await next()
+
+  try {
+    ctx.body = readFileSync(name)
+    ctx.set('Content-Type', mimes[extname(name)] || 'text/plain')
+  } catch {
+    await next()
+  }
 
 }
 
