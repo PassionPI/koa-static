b5d14cdf5d69502d08db905ab40f9d88bbc305fa
PassionPI <472121553@qq.com>
2020-03-12 01:27:42 +0800

    v0.0.6/use stream

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const mimes = require('./mimes')
 const { join, extname } = require('path')
-const { existsSync, readFileSync } = require('fs')
+const { existsSync, createReadStream } = require('fs')
 
 
 const static = (public = '') => async (ctx, next) => {
 
-  const path = ctx.path === '/' ? '/index.html' : ctx.path
-  const name = join(public, path)
-
-  if (existsSync(name)) {
-    try {
-      ctx.body = readFileSync(name)
-      ctx.set('Content-Type', mimes[extname(name)] || 'text/plain')
-    } catch (e) {
-      console.error(e)
+  if (['HEAD', 'GET'].includes(ctx.method)) {
+    const path = ctx.path === '/' ? '/index.html' : ctx.path
+    const name = join(public, path)
+  
+    if (existsSync(name)) {
+      try {
+        ctx.body = createReadStream(name)
+        ctx.type = mimes[extname(name)] || 'text/plain'
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 
